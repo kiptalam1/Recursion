094873bcaf23602584958b19239cf4944a27e558
adams <adamskiptalam0@gmail.com>
2025-01-13 16:01:42 +0300

    Uncomment iterative method

--- fibonacci.js
+++ fibonacci.js
@@ -4,28 +4,28 @@
 
 
 // iterative method
-// function fib(digits) {
-//     let sequence = [0, 1];
-//     // base cases
-//     if (digits <= 0) return [];
-//     if (digits === 1) return [0];
-//     if (digits === 2) return sequence;
+function fib(digits) {
+    let sequence = [0, 1];
+    // base cases
+    if (digits <= 0) return [];
+    if (digits === 1) return [0];
+    if (digits === 2) return sequence;
 
-//     // rest of the sequence
-//     for (let i = 2; i < digits; i++) {
-//         let num = sequence[i - 1] + sequence[i - 2];
-//         sequence.push(num);
-//     }
-//     return sequence;
-// }
+    // rest of the sequence
+    for (let i = 2; i < digits; i++) {
+        let num = sequence[i - 1] + sequence[i - 2];
+        sequence.push(num);
+    }
+    return sequence;
+}
 
-// console.log(fib(2));
-// console.log(fib(5));
-// console.log(fib(6));
-// console.log(fib(1));
-// console.log(fib(8));
-// console.log(fib(0));
-// console.log(fib(-2));
+console.log(fib(2));
+console.log(fib(5));
+console.log(fib(6));
+console.log(fib(1));
+console.log(fib(8));
+console.log(fib(0));
+console.log(fib(-2));
 
 // recursive method.
 function fibRec(digits) {
